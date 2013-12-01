d0b27e2f234b0777464a861303f54c8c7728670d
Chandan Choubey <chandanwtb2009@gmail.com>
2013-12-01 15:05:08 +0530

    first attandance commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Attandance::Application.config.secret_token = '8f8938fa27ac33a33ba561ebdf99230affc5cd2d7b512e63faae2d9c23d0a6a628345053e39a55877a04e856b07cecae879baf71e88a670a3c768e04685eefb2'
